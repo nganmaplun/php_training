e604660acec019f58f181383e256e26ce4627880
ngan-nguyen <ngan-nguyen@dimage.co.jp>
2021-04-27 16:07:37 +0700

    Add basic Login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
+            $table->string('avatar')->nullable();
+            $table->string('desc')->nullable();
+            $table->string('role')->nullable(); //tạm thời
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
