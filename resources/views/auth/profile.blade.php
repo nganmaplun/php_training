132f637d1720808f5171f19fe5660eba05f0f9c0
ngan-nguyen <ngan-nguyen@dimage.co.jp>
2021-05-11 16:43:27 +0700

    Modifie Profile controller

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -5,41 +5,29 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __(' Profile') }}</div>
-                <div class="card-body">
-                    <img width="100px" height="100px" src="{{ asset('images/avatars/'.$user->avatar) }}">
+                <div class="card-body text-center">
+                    <div class="text-center">
+                        <img width="100px" height="100px" src="{{ asset('images/avatars/'.$user->avatar) }}">
+                    </div>
                     
-                    <form enctype="multipart/form-data" method="POST" action="{{ route('profile') }}" >
-                        @csrf
-                        @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                        @endif
+                    <form enctype="multipart/form-data">
                         <div>
-                            <input type="file" name="avatar" class="form-controll">
-                        </div>
-                        
-                        <div>
-                            <label for="name" >{{ __('Name') }}</label>
-                            <input type="name" name="name" class="form-control"  value="{{ $user->name }}">
+                            <h5 for="name" >{{ __('Name') }}</h5>
+                            <p> {{ $user->name }}</p>
                         </div>
                         <div>
-                            <label for="email" >{{ __('Email') }}</label>
-                            <input type="email" name="email" class="form-control"  value="{{ $user->email }}">
+                            <h5 for="email" >{{ __('Email') }}</h5>
+                            <p> {{ $user->email }}</p>
                         </div>
                         <div>
-                            <label for="desc" >{{ __('Desction') }}</label>
-                            <input type="desc" name="desc" class="form-control" value="{{ $user->desc }}">
+                            <h5 for="desc" >{{ __('Desction') }}</h5>
+                            <p> {{ $user->desc }}</p>
                         </div>
                         <br>
-                        <div>
-                            <button type="submit" class="btn btn-success">Update Profile</button>
+                        <div class="col-md-12 text-center">
+                            <a class="btn btn-link pull-right" href="{{ route('profile.edit') }}"><i class="fas fa-backward"></i> Edit Profile</a>
                         </div>
-                    
+                        @include('shared.error')
                 </div>
             </div>
         </div>
